5e3a7a64710659c9f07b155bedac2cfe481aa37e
Ignacio Manuel Baez <ignaciomanuelbaez@hotmail.com>
2024-02-29 17:48:45 -0300

    change npm start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 //arranque de la app, llama a express, llama a la db, encargado de arrancar todo
 import app from "./app.js";
+import "dotenv/config.js";
 import { connectDB } from "./db.js";
 
 connectDB();
 
 app.listen(3000, () => {
-  console.log("Server on http://localhost:3000");
+  console.log(`Server on http://localhost:${process.env.PORT}`);
 });
