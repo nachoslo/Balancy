642d5c196072969beace875e0bde8cd656f6486e
Ignacio Manuel Baez <ignaciomanuelbaez@hotmail.com>
2024-03-02 21:49:07 -0300

    samesite none

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,7 +62,7 @@ export const login = async (req, res) => {
       }
     );
 
-    res.cookie("token", token, { httpOnly: false });
+    res.cookie("token", token, { sameSite: "none" });
 
     res.json({
       name: userFound.name,
